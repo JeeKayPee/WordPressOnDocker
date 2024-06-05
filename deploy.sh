7d95a8c9ebf206ae17fb392c591ee2ddcbe6fec2
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-05 11:37:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install docker.io --yes
-sudo apt install docker-compose --yes
-sudo docker-compose -f stack.yml up
+sudo apt install docker compose --yes
+sudo docker compose -f stack.yml up
